d731e8cb483d531a2fef222b5f62eba59100f708
捕まったひよこ <138089174+Ponzu-IKA@users.noreply.github.com>
2024-10-12 22:40:02 +0900

    レーザーエネルギーを埋め込みで表示するように変更

--- src/main/kotlin/ponzu_ika/utilbot/SlashCommands.kt
+++ src/main/kotlin/ponzu_ika/utilbot/SlashCommands.kt
@@ -1,7 +1,10 @@
 package ponzu_ika.utilbot
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.awt.Color
+import kotlin.math.max
 
 class SlashCommands :ListenerAdapter(){
 
@@ -44,12 +47,38 @@ class SlashCommands :ListenerAdapter(){
                 val redCE = redCount*ce*100
                 val whiteCE = whiteCount*ce*1000
 
+                val allRGBLaserCount = blueCount + greenCount + redCount
+
                 val cePerTick = blueCE + redCE + greenCE + whiteCE
 
                 val efficiency = energy/cePerTick
+                //"青レーザー本数: $blueCount \n緑レーザー本数: $greenCount \n赤レーザー本数: $redCount \n白レーザー本数: $whiteCount \n総エネルギー量: ${Util().roundAndString(energy,4)}\n消費エネルギー: ${Util().roundAndString(cePerTick,2)} CE\\t\n変換効率: ${Util().roundAndString(efficiency,4)}"
+                val eb = EmbedBuilder()
+                eb.setTitle("エネルギー量: **${Util().roundAndString(energy,4)}/t**")
+                eb.setDescription("変換効率: $${Util().roundAndString(efficiency,4)}\n消費エネルギー: ${Util().roundAndString(cePerTick,2)} CE/t")
+                eb.addField("青レーザー本数","$blueCount 本",false)
+                eb.addField("緑レーザー本数","$greenCount 本",false)
+                eb.addField("赤レーザー本数","$redCount 本",false)
 
-                event.reply("青レーザー本数: $blueCount \n緑レーザー本数: $greenCount \n赤レーザー本数: $redCount \n白レーザー本数: $whiteCount \n総エネルギー量: ${Util().roundAndString(energy,4)}\n消費エネルギー: ${Util().roundAndString(cePerTick,2)} CE\\t\n変換効率: ${Util().roundAndString(efficiency,4)}").queue()
+
+                val max = allRGBLaserCount/max(redCount, max(greenCount,blueCount))
+                eb.setColor(Color(255*redCount/allRGBLaserCount*max,255*greenCount/allRGBLaserCount*max,255*blueCount/allRGBLaserCount*max))
+
+                event.replyEmbeds(eb.build()).queue()
             }
         }
     }
+}
+
+fun main () {
+    val r = 4.0
+    val g = 8.0
+    val b = 2.0
+
+    val all = r + g + b
+
+    val maxValue = max(r, max(g,b))
+    val max = all/maxValue
+
+    println("r:${255*r/all*max}, g:${255*g/all*max}, b:${255*b/all*max}")
 }
